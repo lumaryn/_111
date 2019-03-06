30d648aeea19acc2d1e71087deb921c6b4e7f3ca
pochter123 <pochter123@gmail.com>
2019-03-06 14:35:19 +0300

    read file

---
+++ _111/src/main/java/test.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class test {
+
+    public static void main(String[] args) {
+        String s;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\admin\\Desktop\\test.txt"));
+            while ((s = br.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка");
+        }
+    }
+}
